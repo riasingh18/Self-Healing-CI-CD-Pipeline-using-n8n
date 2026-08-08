22a698004bf87c126a73e1f3dd5d9cbd43545f91
riasingh18 <iriaasingh@gmail.com>
2026-08-08 22:22:22 +0530

    First

---
+++ test.js
@@ -0,0 +1,28 @@
+  const express = require('express');
+  const http = require('http');
+
+  const app = express();
+  app.get('/', (req, res) => res.send('Hello from Express App'));
+
+  const server = app.listen(5000, () => {
+    console.log('Server started, running smoke test...');
+
+    http.get('http://localhost:5000/', (res) => {
+        console.log(`Status: ${res.statusCode}`);
+        if(res.statusCode == 200){
+            console.log('Smoke test passed');
+            server.close();
+            process.exit(0);
+
+        }
+
+        else{
+            server.close();
+            process.exit(1);
+        }
+    }).on('error', (err) => {
+        console.error('Request failed:', err.message);
+        server.close();
+        process.exit(1);
+    })
+  });
